b2ff1b653589c464537ff151c49d950e40794d8f
Vitor Miguel <123606610+VtrMiguel@users.noreply.github.com>
2023-01-26 19:31:30 -0300

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,10 @@
+function log(){
+    alert("Cadastro feito com sucesso!");
+    window.open("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index.html")
+    window.close("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index2.html")
+  }
+
+function log2(){
+    window.open("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index.html")
+    window.close("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index2.html")
+  }
