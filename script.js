f5220f4b63066389c56eb8a5fdf56e0a6fc1f583
Vitor Miguel <123606610+VtrMiguel@users.noreply.github.com>
2023-01-26 19:31:25 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function res(){
+    window.open("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index2.html")
+    window.close("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index.html")
+  }
+
+  function res2(){
+    window.open("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index3.html")
+    window.close("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index.html")
+  }
