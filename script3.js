cf83a28a0cc16e5199ebead06992c93f765bcc30
Vitor Miguel <123606610+VtrMiguel@users.noreply.github.com>
2023-01-26 19:31:33 -0300

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,5 @@
+function logg(){
+    alert("E-mail de recuperação enviado!");
+    window.open("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index.html")
+    window.close("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index3.html")
+}
