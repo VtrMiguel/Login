a7419d68a2941f662820abf4a883f0102e0c9311
Vitor Miguel <123606610+VtrMiguel@users.noreply.github.com>
2023-01-28 12:51:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function res(){
     window.open("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index3.html")
     window.close("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index.html")
   }
+
+  function logar(){
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "vitor@gmail.com" && senha == "12345" || login == "rita@md.com" && senha == "rt123"){
+        alert('Sucesso');
+        location.href = "file:///C:/Users/Backoffice/OneDrive/%C3%81rea%20de%20Trabalho/00/index4.html";
+    }else{
+        alert('Usuario ou senha incorretos');
+    }
+}
