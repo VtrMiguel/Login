5bd96d89fd56b8db0992797cca822a2a47d92f0e
Vitor Miguel <123606610+VtrMiguel@users.noreply.github.com>
2023-01-28 12:52:04 -0300

    Create script4.js

---
+++ script4.js
@@ -0,0 +1,5 @@
+function bye(){
+    alert("Até a próxima!");
+    window.open("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index.html")
+    window.close("file:///C:/Users/vitor/OneDrive/Documentos/GitHub/Login/index4.html")
+}
