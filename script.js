aa691a432b2231c10917cf1bd4899e7bea084623
Vitor Miguel <123606610+VtrMiguel@users.noreply.github.com>
2023-01-28 13:00:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function res(){
-    window.open("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index2.html")
-    window.close("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index.html")
+  window.open("file:///C:/Users/Backoffice/OneDrive/%C3%81rea%20de%20Trabalho/00/index2.html")
+  window.close("file:///C:/Users/Backoffice/OneDrive/%C3%81rea%20de%20Trabalho/00/index.html")
+}
+
+function res2(){
+  window.open("file:///C:/Users/Backoffice/OneDrive/%C3%81rea%20de%20Trabalho/00/index3.html")
+  window.close("file:///C:/Users/Backoffice/OneDrive/%C3%81rea%20de%20Trabalho/00/index.html")
+}
+
+function logar(){
+
+  var login = document.getElementById('login').value;
+  var senha = document.getElementById('senha').value;
+
+  if(login == "vitor@gmail.com" && senha == "12345" || login == "rita@md.com" && senha == "rt123"){
+      alert('Sucesso');
+      location.href = "file:///C:/Users/vitor/OneDrive/Documentos/GitHub/Login/index4.html";
+  }else{
+      alert('Usuario ou senha incorretos');
   }
-
-  function res2(){
-    window.open("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index3.html")
-    window.close("file:///C:/Users/vitor/OneDrive/%C3%81rea%20de%20Trabalho/login/index.html")
-  }
-
-  function logar(){
-
-    var login = document.getElementById('login').value;
-    var senha = document.getElementById('senha').value;
-
-    if(login == "vitor@gmail.com" && senha == "12345" || login == "rita@md.com" && senha == "rt123"){
-        alert('Sucesso');
-        location.href = "file:///C:/Users/Backoffice/OneDrive/%C3%81rea%20de%20Trabalho/00/index4.html";
-    }else{
-        alert('Usuario ou senha incorretos');
-    }
 }
